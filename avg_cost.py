b9f36b4746b10896b678466000d34ea6433b58f9
Tarric Sookdeo <tarricsookdeo@outlook.com>
2020-05-02 21:53:59 -0400

    Add function to sort trades by datetime

--- avg_cost.py
+++ avg_cost.py
@@ -21,3 +21,10 @@ trade1 = Trade('KO', 'BUY', datetime1, 5, 50.00)   # [50.00, 5]
 trade2 = Trade('KO', 'BUY', datetime2, 5, 55.00)   # [52.50, 10]
 trade3 = Trade('KO', 'SELL', datetime3, 8, 58.00)  # [55.00, 2]
 trade4 = Trade('KO', 'BUY', datetime4, 5, 52.00)   # [52.86, 7]
+
+trades = [trade2, trade4, trade3, trade1]
+
+
+def sort_trades_by_datetime(trades_list):
+    """Returns a list of trades sorted by their datetime in assending order."""
+    return sorted(trades_list, key=lambda trade: trade.datetime)
