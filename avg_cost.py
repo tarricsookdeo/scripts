0c46c633a9837c620eb02ec2f58e4a9a4e94277f
Tarric Sookdeo <tarricsookdeo@outlook.com>
2020-05-02 21:03:52 -0400

    Add comments next to trade for the calculation of [avg_cost_per_share, shares] after trade is calculated

--- avg_cost.py
+++ avg_cost.py
@@ -17,7 +17,7 @@ datetime3 = datetime.datetime(2020, 1, 3, 9, 30)
 datetime4 = datetime.datetime(2020, 1, 4, 9, 30)
 
 # define Trade objects to test on
-trade1 = Trade('KO', 'BUY', datetime1, 5, 50.00)
-trade2 = Trade('KO', 'BUY', datetime2, 5, 55.00)
-trade3 = Trade('KO', 'SELL', datetime3, 8, 58.00)
-trade4 = Trade('KO', 'BUY', datetime4, 5, 52.00)
+trade1 = Trade('KO', 'BUY', datetime1, 5, 50.00)   # [50.00, 5]
+trade2 = Trade('KO', 'BUY', datetime2, 5, 55.00)   # [52.50, 10]
+trade3 = Trade('KO', 'SELL', datetime3, 8, 58.00)  # [55.00, 2]
+trade4 = Trade('KO', 'BUY', datetime4, 5, 52.00)   # [52.86, 7]
